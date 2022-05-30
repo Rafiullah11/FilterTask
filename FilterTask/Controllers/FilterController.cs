462a14b02804ddb16e2e9073712104d1ad702500
Rafiullah <rafiullahafridi11@yahoo.com>
2022-05-30 18:49:16 +0500

    Add project files.

---
+++ FilterTask/Controllers/FilterController.cs
@@ -0,0 +1,24 @@
+﻿using FilterTask.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FilterTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class FilterController : Controller
+    {
+        // GET: api/<FilterController>
+
+        [HttpGet]
+        [ServiceFilter(typeof(CustomFilterAttribute))]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+       
+    }
+}
---
+++ FilterTask/Models/CustomFilterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace FilterTask.Models
+{
+    public class CustomFilterAttribute : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            
+            context.HttpContext.Response.Headers.Add("action-namename", context.ActionDescriptor.DisplayName);
+            context.HttpContext.Response.Headers.Add("http-method", context.HttpContext.Request.Method);
+            context.HttpContext.Response.Headers.Add("http-scheme", context.HttpContext.Request.Scheme);
+            context.HttpContext.Response.Headers.Add("host", context.HttpContext.Request.Host.ToString());
+            context.HttpContext.Response.Headers.Add("port", context.HttpContext.Request.Host.Port.ToString());
+            context.HttpContext.Response.Headers.Add("excute-time", watch.Elapsed.ToString());
+            context.HttpContext.Response.Headers.Add("server-date", context.HttpContext.Response.Headers.Date.ToString());
+            watch.Stop();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+    }
+}
